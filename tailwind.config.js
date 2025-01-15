1ffc4fd3d3321f628a12b2d4607a56572cf0ccbe
TranDanh1122 <trandanh14042000@gmail.com>
2025-01-15 20:02:15 +0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        mb: { min: "0", max: "767px" }
+      }
+    },
+  },
+  plugins: [],
+}
+
